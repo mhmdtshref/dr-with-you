d4b1c49334c45692879fb7c0432b789a2dd48056
Mohamed <mshareef@kiitos-tech.com>
2020-04-17 16:26:02 +0300

    Setup database connection (using sequelize)

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,23 @@ dotenvFlow.config({ path: './environment' });
 
 import app from './app';
 import http from 'http';
+import { sequelize } from './models';
 
 const httpServer = new http.Server(app);
 
 (async () => {
   try {
-    // TODO: Connect to sequelize.
+    sequelize.authenticate();
+    await sequelize.sync({
+      force: false,
+    });
+    console.log('Sequelize Synced with PostgreSQL Database');
   } catch (error) {
     console.log('Sequelize connection error: ', error.message);
   }
 
-  const port = process.env.PORT;
+  const { PORT: port, HOST: host } = process.env;
   httpServer.listen(port, () => {
-    console.log(`Server is running on: http://localhost:${port}`);
+    console.log(`Server is running on: http://${host}:${port}`);
   });
 })();
