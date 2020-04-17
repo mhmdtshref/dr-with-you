70308a575aeac7fdbdf759176a3bf04093b6cf79
Mohamed <mshareef@kiitos-tech.com>
2020-04-17 10:43:01 +0300

    Fix by eslint

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,6 @@ import express from 'express';
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,14 @@ import http from 'http';
 const httpServer = new http.Server(app);
 
 (async () => {
-    try {
-        // TODO: Connect to sequelize.
-    } catch(error) {
-        console.log('Sequelize connection error: ', error.message);
-    }
+  try {
+    // TODO: Connect to sequelize.
+  } catch (error) {
+    console.log('Sequelize connection error: ', error.message);
+  }
 
-    const port = process.env.PORT;
-    httpServer.listen(port, () => {
-        console.log(`Server is running on: http://localhost:${port}`);
-    })
-
-}
+  const port = process.env.PORT;
+  httpServer.listen(port, () => {
+    console.log(`Server is running on: http://localhost:${port}`);
+  });
+})();
