35b352fdb61b2d70d75e29c636850919727371f1
Mohamed <mshareef@kiitos-tech.com>
2020-04-17 16:25:38 +0300

    Add models to idex file and sequelize

---
+++ src/models/index.ts
@@ -0,0 +1,21 @@
+import sequelize from './sequelize';
+import User from './User';
+import Specialty from './Specialty';
+import Test from './Test';
+import Doctor from './Doctor';
+import Review from './Review';
+import Question from './Question';
+import Option from './Option';
+import Answer from './Answer';
+
+export {
+    sequelize,
+    User,
+    Specialty,
+    Test,
+    Doctor,
+    Review,
+    Question,
+    Option,
+    Answer
+};
---
+++ src/models/sequelize.ts
@@ -0,0 +1,30 @@
+import { Sequelize, Options } from 'sequelize';
+
+const { 
+    DATABASE_HOST: databaseHost = '',
+    DATABASE_NAME: databaseName = '',
+    DATABASE_USER: databaseUser = '',
+    DATABASE_PASSWORD: databasePassword = '',
+    DATABASE_SSL_ENABLED: databaseSslEnabled = 'false',
+} =  process.env;
+
+let databaseOptions: Options = {
+    dialect: 'postgres',
+    host: databaseHost,
+};
+
+if(databaseSslEnabled === 'true') {
+    databaseOptions = {
+        ...databaseOptions,
+        ssl: true,
+        dialectOptions: {
+            ssl: {
+                require: true,
+            },
+        },
+    }
+}
+
+const sequelize = new Sequelize(databaseName, databaseUser, databasePassword, databaseOptions);
+
+export default sequelize;
